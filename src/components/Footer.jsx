c11f74808d02cfedff5889b078299b30fdbebbaa
Marius <marius.sergent@gmail.com>
2023-03-22 09:14:01 +0100

    Ajout d'une card et affichage des restaurants

---
+++ src/components/Footer.jsx
@@ -0,0 +1,37 @@
+import '../styles/FooterStyle.css'
+
+function Footer() {
+  return ( 
+    <footer className="footer">
+      <div className="col">
+        <ul>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+        </ul>
+      </div>
+      <div className="col">
+        <ul>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+        </ul>
+      </div>
+      <div className="col">
+        <ul>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+          <li>toto</li>
+        </ul>
+      </div>
+    </footer>
+   );
+}
+
+export default Footer;
