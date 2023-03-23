65257b3dd531a34590e6435abc62309e2c1b8a4c
Marius <marius.sergent@gmail.com>
2023-03-23 11:36:59 +0100

    Ajout de commentaires

--- src/components/DishesList.jsx
+++ src/components/DishesList.jsx
@@ -1,11 +1,14 @@
 function DishesList ({ dishes }) {
-  console.log(dishes)
+  // On prépare la liste des plats (un tableau vide)
   const elements = []
   for (const dish of dishes.data) {
+    // Pour chaque plat de la liste, on ajoute un composant visuel (ici un h1) dans le tableau
     elements.push(<h1>{dish.attributes.name}</h1>)
   }
+  // On obtient dans "elements" un tableau de composants React
   return dishes && dishes.data && (
     <div className='list'>
+      {/* On affiche le tableau contenant les plats */}
       {elements}
     </div>
   )
